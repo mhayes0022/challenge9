ca4dd737c3f5e097845bbb10ad3a5c5f5df7b04c
MHayes <mhayes0022@gmail.com>
2024-03-17 19:05:28 -0500

    wrote list of questions in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What is a brief description of your project?',
+    },
+    {
+      type: 'input',
+      name: 'instructions',
+      message: 'What are your installation instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage information for the project?',
+    },
+    {
+      type: 'input',
+      name: 'contribution guidelines',
+      message: 'What are the contribution guidelines for the project?',
+    },
+    {
+      type: 'input',
+      name: 'testing',
+      message: 'What are the testing instructions for the project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What kind of license would you like to include?',
+      choices: [''],
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+     },
+];
+  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,5 +62,3 @@ function init() {}
 
 // Function call to initialize app
 init();
-
-//testing
